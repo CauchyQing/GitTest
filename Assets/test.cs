2e04c5e46b1e3055d076d075c482e672e5b861ae
卿柯序 <1415708632@qq.com>
2023-04-03 23:06:40 +0800

    第三次

--- Assets/test.cs
+++ Assets/test.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    private string s = "Second";
+    private string s = "Third";
     // Start is called before the first frame update
     void Start()
     {
